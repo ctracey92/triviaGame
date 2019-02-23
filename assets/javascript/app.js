703d869d306d7e4de4044552c400af929111f083
Cody <cody.tracy92@gmail.com>
2019-02-23 16:08:15 -0600

    added the submit button end game function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,6 +2,9 @@
 var time = 60;
 var running = true; 
 var intervalID;
+var submitted = false;
+var userAnswers = [];
+var answers = ["1","1","1","1","1"]
 
 //Game running varaible
 var gameRunning;
@@ -39,6 +42,42 @@ if (time > 0){
 else if (time <= 0){
     gameRunning = false;
 }
+
+//When Submitted pushes answers into an array and runs endGame function
+$("#submitButton").on("click", function answers(){
+    var answer1 = $( "input[type=radio][name=question1]:checked" ).val()
+    var answer2 = $( "input[type=radio][name=question2]:checked" ).val()
+    var answer3 = $( "input[type=radio][name=question3]:checked" ).val()
+    var answer4 = $( "input[type=radio][name=question4]:checked" ).val()
+    var answer5 = $( "input[type=radio][name=question5]:checked" ).val()
+userAnswers.push(answer1, answer2, answer3, answer4, answer5);
+console.log(userAnswers);
+submitted = true;
+if (submitted === true){
+    endGame();
+    alert("Game Over");
+}
+})
+
+
+//Checks to see which answers were right and which were wrong
+function endGame(){
+    for (i = 0; i < answers.length; i++)
+        if (userAnswers[i] === answers[i]){
+            correct++
+        }
+        else {
+            incorrect++
+        }
+    $("#correctAnswers").text("Correct: " + correct);
+    $("#incorrectAnswers").text("Incorrect: " + incorrect);
+}
+
+
+
+
+   
+
 
 //Pseudo code
 // while the game is running run the following functions
